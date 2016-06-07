bbdc26f458c1c9b950d98035e33e37f155327983
Sergey Yermochenko <yermochenko@gmail.com>
2016-06-07 14:02:02 +0300

    исправлены ошибки с именами пакетов

--- src/by/vsu/mf/amm/sta/exception/operation/VectorSizeExeption.java
+++ src/by/vsu/mf/amm/sta/exception/operation/VectorSizeExeption.java
@@ -1,6 +1,5 @@
 package by.vsu.mf.amm.sta.exception.operation;
 
-import by.vsu.mf.amm.sta.exception.operation.MatrixSizeException.IndexType;
 import by.vsu.mf.amm.sta.method.operation.Operation;
 
 public class VectorSizeExeption extends OperationException {
